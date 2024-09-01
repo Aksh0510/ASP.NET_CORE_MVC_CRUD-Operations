e602fa6521d981e36648f14c5ab9663b22883428
HP <HP@DESKTOP-VIMD5IS>
2024-09-01 13:35:42 +0530

    Add project files.

---
+++ Models/CustomerModel/AddCutomerViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ASPNET_CORE_MVC_CRUD.Models.CustomerModel
+{
+    public class AddCutomerViewModel
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Phone { get; set; }
+        public required string Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
